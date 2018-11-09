497c2d2b526ee35e4483cf0b14aa45503a0cc38c
Danylo <dshyshla@gmail.com>
2018-11-09 21:49:01 +0200

    Realisation of main.cpp for Lab 4 on C++

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "waterpump.h"
+#include <string>
+
+using namespace std;
+
+int main() {
+    WaterPump One(10, "susu", 400, "K-48", "normal", 3, 40, 1, "public_1");
+    WaterPump Two(20, "nihe", 100, "K-39","normal", 3, 40, 1, "public_1");
+    WaterPump Three(30, "koche", 450,"L-65","normal", 3, 40, 1, "public_1");
+    One.GetClass();
+    Two.GetClass();
+    Three.GetClass();
+}
