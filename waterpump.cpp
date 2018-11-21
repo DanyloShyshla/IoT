746c9e46735adab5374d55f316ae7fc82203fee3
DanyloShyshla <dshyshla@gmail.com>
2018-11-21 21:36:26 +0200

    waterpump.cpp
    Variable and class names updated.

--- waterpump.cpp
+++ waterpump.cpp
@@ -8,17 +8,17 @@ WaterPump::WaterPump() {
     this->name = "Name";
 }
 
-WaterPump::WaterPump(int powerConsumptions, string Brand, int waterCapacity, string Name, string Type, int amount,
-                     int LengthOfCable, int public_first, string public_second) {
+WaterPump::WaterPump(int powerСnsumptions, string Brand, int waterCapacity, string Name, string Type, int amount,
+                     int LengthOfCable, int publicFirst, string publicSecond) {
     this->name = Name;
     this->type = Type;
     this->amount = amount;
     this->lengthOfCable = LengthOfCable;
     this->brand = Brand;
     this->waterCapacity = waterCapacity;
-    this->powerConsumptions = powerConsumptions;
-    this->public_first = public_first;
-    this->public_second = public_second;
+    this->powerConsumptions = powerСnsumptions;
+    this->publicFirst = publicFirst;
+    this->publicSecond = publicSecond;
 }
 
 WaterPump::~WaterPump() {
@@ -29,8 +29,8 @@ void WaterPump::getClass() {
 
     std::cout << "Name: " << name << "\n" << "Type: " << type << "\n" << "Amount: " << amount << "\n"
               << "LengthOfCable: " << lengthOfCable << "\n" << "Brand: " << brand << "\n" << "waterCapacity: "
-              << waterCapacity << "\n" << "powerConsumptions: " << powerConsumptions << "\n" << "public_first: "
-              << public_first << "\n" << "public_second: " << public_second << "\n" << std::endl;
+              << waterCapacity << "\n" << "powerСnsumptions: " << powerConsumptions << "\n" << "publicFirst: "
+              << publicFirst << "\n" << "publicSecond: " << publicSecond << "\n" << std::endl;
 }
 
 int WaterPump::getPowerConsumptions() {
