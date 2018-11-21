f7c49164c991095713a17d05e181947e6c227166
DanyloShyshla <dshyshla@gmail.com>
2018-11-21 21:03:30 +0200

    main.cpp
    Variable and class names updated.

--- main.cpp
+++ main.cpp
@@ -6,9 +6,9 @@ using namespace std;
 
 int main() {
     WaterPump One(10, "susu", 400, "K-48", "normal", 3, 40, 1, "public_1");
-    WaterPump Two(20, "nihe", 100, "K-39","normal", 3, 40, 1, "public_1");
-    WaterPump Three(30, "koche", 450,"L-65","normal", 3, 40, 1, "public_1");
-    One.GetClass();
-    Two.GetClass();
-    Three.GetClass();
+    WaterPump Two(20, "nihe", 100, "K-39", "normal", 3, 40, 1, "public_1");
+    WaterPump Three(30, "koche", 450, "L-65", "normal", 3, 40, 1, "public_1");
+    One.getClass();
+    Two.getClass();
+    Three.getClass();
 }
